57796e1958d299565b882e9d87c01332c0921689
henryflia <superflia2059@gmail.com>
2025-11-16 22:14:35 -0400

    format

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,6 @@
 import ShoppingCart from "./ShoppingCart.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-const cart = new ShoppingCart("so-cart", ".product-list")
-loadHeaderFooter()
+const cart = new ShoppingCart("so-cart", ".product-list");
+loadHeaderFooter();
 cart.renderCartContents();
-
