8630fc8998abc058dd1b1ffa6b5dc0c86b55efa5
henryflia <superflia2059@gmail.com>
2025-11-26 21:14:10 -0400

    format

--- src/js/product_listing.js
+++ src/js/product_listing.js
@@ -1,6 +1,6 @@
 import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter, getParam} from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
